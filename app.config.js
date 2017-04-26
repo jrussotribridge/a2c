e912b0bef3c1aa5fe0912049e527a01385d95166
Justin Russo <Justin.Russo@tribridge.com>
2017-04-26 16:11:45 -0400

    Newest

---
+++ app.config.js
@@ -0,0 +1,3 @@
+import 'style-loader';
+
+require('./node_modules/font-awesome/css/font-awesome.min.css');
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 var glob = require("glob");
 
-var files = glob.sync('./public/app/*.js');
-files = files.concat(glob.sync('./public/app/controllers/*.js'));
-
-files.push('webpack/hot/dev-server');
+var files = ['./app.config.js'];
+/*files = files.concat(glob.sync('./public/app/*.js'));
+files = files.concat(glob.sync('./public/app/controllers/*.js'));*/
 
 module.exports = {
     entry: files,
     output: {
         path: __dirname + '/public/bundles/',
-        filename: 'app.bundle.js',
-        publicPath: '/public/'
+        filename: 'app.bundle.js'
     },
     module: {
-        loaders: [
-            { 
-            }
-        ]
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style!css?sourceMap'
+        }, {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/octet-stream"
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file"
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=image/svg+xml"
+        }]
     }
 };
